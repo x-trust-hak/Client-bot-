d13e0646c236a48ebe56e57f4eeec6814698a2c0
x-trust-hak <luistrust8@gmail.com>
2026-06-24 21:11:43 +0100

    Update case.js

--- src/case.js
+++ src/case.js
@@ -610,7 +610,8 @@ module.exports = async (conn, m, chatUpdate, ctx = {}) => {
 
         switch (command) {
         
-        case 'sound1':
+        
+     case 'sound1':
     case 'sound2':
     case 'sound3':
     case 'sound4':
@@ -619,7 +620,97 @@ module.exports = async (conn, m, chatUpdate, ctx = {}) => {
     case 'sound7':
     case 'sound8':
     case 'sound9':
-    case 'sound10': {
+    case 'sound10':
+    case 'sound11':
+    case 'sound12':
+    case 'sound13':
+    case 'sound14':
+    case 'sound15':
+    case 'sound16':
+    case 'sound17':
+    case 'sound18':
+    case 'sound19':
+    case 'sound20':
+    case 'sound21':
+    case 'sound22':
+    case 'sound23':
+    case 'sound24':
+    case 'sound25':
+    case 'sound26':
+    case 'sound27':
+    case 'sound28':
+    case 'sound29':
+    case 'sound30':
+    case 'sound31':
+    case 'sound32':
+    case 'sound33':
+    case 'sound34':
+    case 'sound35':
+    case 'sound36':
+    case 'sound37':
+    case 'sound38':
+    case 'sound39':
+    case 'sound40':
+    case 'sound41':
+    case 'sound42':
+    case 'sound43':
+    case 'sound44':
+    case 'sound45':
+    case 'sound46':
+    case 'sound47':
+    case 'sound48':
+    case 'sound49':
+    case 'sound50':
+    case 'sound51':
+    case 'sound52':
+    case 'sound53':
+    case 'sound54':
+    case 'sound55':
+    case 'sound56':
+    case 'sound57':
+    case 'sound58':
+    case 'sound59':
+    case 'sound60':
+    case 'sound61':
+    case 'sound62':
+    case 'sound63':
+    case 'sound64':
+    case 'sound65':
+    case 'sound66':
+    case 'sound67':
+    case 'sound68':
+    case 'sound69':
+    case 'sound70':
+    case 'sound71':
+    case 'sound72':
+    case 'sound73':
+    case 'sound74':
+    case 'sound75':
+    case 'sound76':
+    case 'sound77':
+    case 'sound78':
+    case 'sound79':
+    case 'sound80':
+    case 'sound81':
+    case 'sound82':
+    case 'sound83':
+    case 'sound84':
+    case 'sound85':
+    case 'sound86':
+    case 'sound87':
+    case 'sound88':
+    case 'sound89':
+    case 'sound90':
+    case 'sound91':
+    case 'sound92':
+    case 'sound93':
+    case 'sound94':
+    case 'sound95':
+    case 'sound96':
+    case 'sound97':
+    case 'sound98':
+    case 'sound99':
+    case 'sound100':   {
 try {
         let link = `https://raw.githubusercontent.com/TeamTrust123/Songs-/main/${command}.mp3`
         await conn.sendMessage(m.from, {
